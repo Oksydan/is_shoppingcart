23e5576c3d6ab4aa04def929df5aa37e286aaae3
Igor Stępień <igor@istepien.dev>
2023-04-23 23:10:59 +0200

    version bump

--- is_shoppingcart.php
+++ is_shoppingcart.php
@@ -21,7 +21,7 @@ class Is_shoppingcart extends Module
     {
         $this->name = 'is_shoppingcart';
         $this->tab = 'front_office_features';
-        $this->version = '2.0.1';
+        $this->version = '3.0.0';
         $this->author = 'Igor Stępień';
         $this->need_instance = 0;
 
